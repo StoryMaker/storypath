65fc48c638cacdb6d4417be5dae3568b9b62a76b
Micah Lucas <micah@scal.io>
2014-10-07 18:47:01 -0700

    Added more tests.

--- app/src/androidTest/java/scal/io/liger/tests/MainActivityTest.java
+++ app/src/androidTest/java/scal/io/liger/tests/MainActivityTest.java
@@ -9,7 +9,10 @@ import scal.io.liger.R;
 
 import static android.test.ViewAsserts.assertOnScreen;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.hasToString;
 
@@ -21,7 +24,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     private CardUI mCardUI;
 
     public MainActivityTest() {
-        super(MainActivity.class);
+        super("scal.io.liger", MainActivity.class);
     }
 
     protected void setUp() throws Exception {
@@ -31,11 +34,42 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         mCardUI = (CardUI) mMainActivity.findViewById(R.id.cardsview);
     }
 
+    public void testPreConditions() {
+        assertTrue(mMainActivity != null);
+        assertTrue(mCardUI != null);
+    }
+
     public void testCardsUIExist() {
         assertOnScreen(mMainActivity.getWindow().getDecorView(), mCardUI);
     }
 
     public void testLoadJSONFile() {
+        getJSONFile();
+    }
+
+    public void testClickMediumVideo() {
+        getJSONFile();
+        onView(withId(R.id.btn_medium_video)).perform(click());
+    }
+
+    public void testClickMediumAudio() {
+        getJSONFile();
+        onView(withId(R.id.btn_medium_audio)).perform(click());
+    }
+
+    public void testClickMediumPhoto() {
+        getJSONFile();
+        onView(withId(R.id.btn_medium_photo)).perform(click());
+    }
+
+    private void getJSONFile() {
         onData(hasToString(equalToIgnoringCase("learning_guide_library.json"))).perform(click());
+    }
+
+    public void testClickContinue() {
+        getJSONFile();
+        onView(withId(R.id.btn_medium_video)).perform(click());
+        onView(withId(R.id.cardsview)).perform(Util.swipeUp());
+        onView(withText("Got it!")).perform(click());
     }
 }
