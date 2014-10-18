b557b311d49e1de1619596927769436f04cc6306
micah <micahjlucas@gmail.com>
2014-10-17 22:55:17 -0700

    Added milestone progress card.

--- lib/src/main/java/scal/io/liger/view/MilestoneProgressCardView.java
+++ lib/src/main/java/scal/io/liger/view/MilestoneProgressCardView.java
@@ -7,30 +7,27 @@ import android.widget.TextView;
 
 import scal.io.liger.model.Card;
 import scal.io.liger.R;
-import scal.io.liger.model.ProgressCard;
+import scal.io.liger.model.MilestoneProgressCard;
 
 /**
  * Created by josh on 8/8/14.
  */
-public class ProgressCardView implements DisplayableCard {
-    private ProgressCard mCardModel;
+public class MilestoneProgressCardView implements DisplayableCard {
+    private MilestoneProgressCard mCardModel;
     private Context mContext;
 
-    public ProgressCardView(Context context, Card cardModel) {
+    public MilestoneProgressCardView(Context context, Card cardModel) {
         mContext = context;
-        mCardModel = (ProgressCard) cardModel;
+        mCardModel = (MilestoneProgressCard) cardModel;
     }
 
     @Override
     public View getCardView(Context context) {
-        // StoryPath spm = mCardModel.getStoryPathReference();
-
         if (mCardModel == null) {
             return null;
         }
 
-        View view = LayoutInflater.from(context).inflate(R.layout.card_progress, null);
-
+        View view = LayoutInflater.from(context).inflate(R.layout.card_milestone_progress, null);
         ((TextView) view.findViewById(R.id.tv_text)).setText(mCardModel.getFilledCount() + " / " + mCardModel.getMaxCount());
 
         // supports automated testing
