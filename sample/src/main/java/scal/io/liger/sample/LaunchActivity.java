c7a9b186e7ef601974c75380304d22a261171de8
David Brodsky <davidpbrodsky@gmail.com>
2014-11-12 14:09:07 -0800

    Cleanup sample module

--- sample/src/main/java/scal/io/liger/sample/LaunchActivity.java
+++ sample/src/main/java/scal/io/liger/sample/LaunchActivity.java
@@ -21,27 +21,4 @@ public class LaunchActivity extends Activity {
         i.putExtra("photo_essay_slide_duration", 5000);
         startActivity(i);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launch, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
