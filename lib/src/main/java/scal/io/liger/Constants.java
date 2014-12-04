1f16a4df29fb60a28d72ef615700b5fea69e5836
Josh Steiner <josh@vitriolix.com>
2014-12-03 22:39:14 -0800

    add soundrecorder stuff from storymaker

--- lib/src/main/java/scal/io/liger/Constants.java
+++ lib/src/main/java/scal/io/liger/Constants.java
@@ -51,4 +51,6 @@ public class Constants {
 
     public static final String EXTRA_STORY_TITLE = "story_title";
     public static final String EXTRA_EXPORT_CLIPS = "export_clips";
+
+    public final static String DEFAULT_AUDIO_SAMPLE_RATE = "22050";  // FIXME probe if a better audio rate is supported on this device
 }
