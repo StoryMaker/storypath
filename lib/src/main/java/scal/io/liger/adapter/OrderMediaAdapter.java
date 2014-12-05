dcff5c97384d57c05a4f8c39763ffba2b8e9de5e
Micah Lucas <micahjlucas@gmail.com>
2014-12-04 21:48:34 -0800

    Updated Audio drawable icon.

--- lib/src/main/java/scal/io/liger/adapter/OrderMediaAdapter.java
+++ lib/src/main/java/scal/io/liger/adapter/OrderMediaAdapter.java
@@ -158,22 +158,12 @@ public class OrderMediaAdapter extends RecyclerView.Adapter<OrderMediaAdapter.Vi
             } else if (mMedium.equals(Constants.PHOTO)) {
                 viewHolder.thumbnail.setImageURI(mediaURI);
                 return;
+            } else if (mMedium.equals(Constants.AUDIO)) {
+                int drawable = R.drawable.audio_waveform;
+                viewHolder.thumbnail.setImageDrawable(context.getResources().getDrawable(drawable));
+                return;
             }
         }
-
-        //handle fall-through cases: (media==null || medium==AUDIO)
-
-        String clipType = ccm.getClipType();
-        int drawable = R.drawable.ic_launcher;
-
-        if (clipType.equals(Constants.CHARACTER)) {
-            drawable = R.drawable.cliptype_close;
-        } else if (clipType.equals(Constants.ACTION)) {
-            drawable = R.drawable.cliptype_medium;
-        } else if (clipType.equals(Constants.RESULT)) {
-            drawable = R.drawable.cliptype_long;
-        }
-        viewHolder.thumbnail.setImageDrawable(context.getResources().getDrawable(drawable));
     }
 
     @Override
