79c63af9019b562013a4f91e670d7827dee0ff42
Josh Steiner <josh@vitriolix.com>
2014-12-09 16:46:32 -0800

    fix crash when we dont check for null return from retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);

--- lib/src/main/java/scal/io/liger/model/FullMetadata.java
+++ lib/src/main/java/scal/io/liger/model/FullMetadata.java
@@ -35,8 +35,11 @@ public class FullMetadata implements Parcelable {
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(this.filePath);
         String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-        long timeInmillisec = Long.parseLong( time );
-        this.duration = Util.safeLongToInt(timeInmillisec);
+        long timeMs = 0;
+        if (time != null) {
+            timeMs = Long.parseLong(time);
+        }
+        this.duration = Util.safeLongToInt(timeMs);
     }
 
     public FullMetadata(Parcel in) {
