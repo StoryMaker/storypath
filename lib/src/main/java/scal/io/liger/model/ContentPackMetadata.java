f7df7d241806c6fa4232d3011aef9b60e0a0c4a6
mnbogner <mnbogner@eds.org>
2015-02-19 14:35:10 -0800

    added initial support for content pack metadata files (currently contains only a thumbnail path)

---
+++ lib/src/main/java/scal/io/liger/model/ContentPackMetadata.java
@@ -0,0 +1,17 @@
+package scal.io.liger.model;
+
+/**
+ * Created by mnbogner on 2/19/15.
+ */
+public class ContentPackMetadata {
+
+    String contentPackThumbnailPath; // path of actual thumbnail within content pack zip
+
+    public String getContentPackThumbnailPath() {
+        return contentPackThumbnailPath;
+    }
+
+    public void setContentPackThumbnailPath(String contentPackThumbnailPath) {
+        this.contentPackThumbnailPath = contentPackThumbnailPath;
+    }
+}
