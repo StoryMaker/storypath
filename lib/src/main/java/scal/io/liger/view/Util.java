36c10c51aa8e9440b8bf763d157047b5f9930c2b
mnbogner <mnbogner@eds.org>
2015-08-11 15:28:46 -0700

    improved error handling to prevent null values from causing crashes during publication

--- lib/src/main/java/scal/io/liger/view/Util.java
+++ lib/src/main/java/scal/io/liger/view/Util.java
@@ -49,24 +49,29 @@ public class Util {
     }
 
     public static void startPublishActivity(Activity host, StoryPath storyPath) {
-        ArrayList<FullMetadata> exportMetadata = storyPath.exportSelectedFullMetadata(); // TODO : Place in AsyncTask?
-        ArrayList<AudioClipFull> exportAudioClipsMetadata = storyPath.exportAudioClips();
-        if (exportMetadata.size() > 0) {
-            Intent i = new Intent();
-            i.setAction(Constants.ACTION_PUBLISH);
-            i.putExtra(Constants.EXTRA_STORY_TITLE, storyPath.getTitle());
-            i.putExtra(Constants.EXTRA_STORY_INSTANCE_PATH, storyPath.getStoryPathLibraryFile());
+
+        try {
+            ArrayList<FullMetadata> exportMetadata = storyPath.exportSelectedFullMetadata(); // TODO : Place in AsyncTask?
+            ArrayList<AudioClipFull> exportAudioClipsMetadata = storyPath.exportAudioClips();
+            if (exportMetadata.size() > 0) {
+                Intent i = new Intent();
+                i.setAction(Constants.ACTION_PUBLISH);
+                i.putExtra(Constants.EXTRA_STORY_TITLE, storyPath.getTitle());
+                i.putExtra(Constants.EXTRA_STORY_INSTANCE_PATH, storyPath.getStoryPathLibraryFile());
 //            i.putExtra(Constants.EXTRA_REQUIRED_UPLOAD_TARGETS, storyPath.getRequiredUploadTargets());
 //            i.putExtra(Constants.EXTRA_REQUIRED_PUBLISH_TARGETS, storyPath.getRequiredPublishTargets());
 //            i.putExtra(Constants.EXTRA_REQUIRED_TAGS, storyPath.getRequiredTags());
-            i.putParcelableArrayListExtra(Constants.EXTRA_EXPORT_CLIPS, exportMetadata);
-            if (exportAudioClipsMetadata != null) {
-                i.putParcelableArrayListExtra(Constants.EXTRA_EXPORT_AUDIOCLIPS, exportAudioClipsMetadata);
+                i.putParcelableArrayListExtra(Constants.EXTRA_EXPORT_CLIPS, exportMetadata);
+                if (exportAudioClipsMetadata != null) {
+                    i.putParcelableArrayListExtra(Constants.EXTRA_EXPORT_AUDIOCLIPS, exportAudioClipsMetadata);
+                }
+                host.startActivity(i);
+                host.finish(); // Do we definitely want to finish the host Activity?
+            } else {
+                Toast.makeText(host, host.getString(R.string.this_story_has_no_clips_to_publish), Toast.LENGTH_LONG).show();
             }
-            host.startActivity(i);
-            host.finish(); // Do we definitely want to finish the host Activity?
-        } else {
-            Toast.makeText(host, host.getString(R.string.this_story_has_no_clips_to_publish), Toast.LENGTH_LONG).show();
+        } catch (StoryPath.MediaException me) {
+            Toast.makeText(host, me.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
