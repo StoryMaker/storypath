a113ee931b7860933eafe6036a79edc5e76fdc47
Josh Steiner <josh@vitriolix.com>
2015-09-11 15:53:28 -0700

    purging Utility.getRealPathFromURI and replacing with the more robust FileUtils.getPath

--- lib/src/main/java/scal/io/liger/Utility.java
+++ lib/src/main/java/scal/io/liger/Utility.java
@@ -47,64 +47,6 @@ public class Utility {
         return (s == null) || (s.length() == 0);
     }
 
-    public static String getRealPathFromURI(Context context, Uri contentUri) {
-        if (contentUri == null) {
-            return null;
-        }
-
-        // work-around to handle normal paths
-        if (contentUri.toString().startsWith(File.separator)) {
-            return contentUri.toString();
-        }
-
-        // work-around to handle normal paths
-        if (contentUri.toString().startsWith("file://")) {
-            return contentUri.toString().split("file://")[1];
-        }
-
-        // TODO deal with document providers
-        // path of form : content://com.android.providers.media.documents/document/video:183
-
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }
-
-    /*
-
-
-    public String getRealPathFromURI(Context context, Uri contentUri) {
-        if (contentUri == null)
-            return null;
-
-        // work-around to handle normal paths
-        if (contentUri.toString().startsWith(File.separator)) {
-            return contentUri.toString();
-        }
-
-        Cursor cursor = null;
-        try {
-            String[] proj = { MediaStore.Images.Media.DATA };
-            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }
-     */
     public static void toastOnUiThread(Activity activity, String message) {
         toastOnUiThread(activity, message, false);
     }
