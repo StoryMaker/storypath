ccf024d726d1cdc125f34597a5235158343508e0
n8fr8 <nathan@freitas.net>
2016-05-18 09:38:32 -0400

    latestEventInfo method no longer exists; set the intent directly

--- lib/src/main/java/com/google/android/vending/expansion/downloader/impl/DownloadNotification.java
+++ lib/src/main/java/com/google/android/vending/expansion/downloader/impl/DownloadNotification.java
@@ -134,8 +134,10 @@ public class DownloadNotification implements IDownloaderClient {
             mCurrentTitle = mLabel.toString();
             mCurrentNotification.tickerText = mLabel + ": " + mCurrentText;
             mCurrentNotification.icon = iconResource;
-            mCurrentNotification.setLatestEventInfo(mContext, mCurrentTitle, mCurrentText,
-                    mContentIntent);
+            
+           // mCurrentNotification.setLatestEventInfo(mContext, mCurrentTitle, mCurrentText,
+             //       mContentIntent);
+            mCurrentNotification.contentIntent = mContentIntent;
             if (ongoingEvent) {
                 mCurrentNotification.flags |= Notification.FLAG_ONGOING_EVENT;
             } else {
@@ -156,7 +158,8 @@ public class DownloadNotification implements IDownloaderClient {
             // we just show the text
             mNotification.tickerText = mCurrentTitle;
             mNotification.icon = android.R.drawable.stat_sys_download;
-            mNotification.setLatestEventInfo(mContext, mLabel, mCurrentText, mContentIntent);
+  //          mNotification.setLatestEventInfo(mContext, mLabel, mCurrentText, mContentIntent);
+            mCurrentNotification.contentIntent = mContentIntent;
             mCurrentNotification = mNotification;
         } else {
             mCustomNotification.setCurrentBytes(progress.mOverallProgress);
